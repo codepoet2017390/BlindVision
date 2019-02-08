2bfffdd9c5b966bd246e605e43f8e93144b48064
Rohan <rohan_dce@outlook.com>
2019-02-08 14:56:35 +0530

    DONe

---
+++ src/org/tensorflow/demo/env/InterfaceContext.java
@@ -0,0 +1,8 @@
+package org.tensorflow.demo.env;
+
+import android.content.Context;
+
+public interface InterfaceContext {
+
+    public Context getContext();
+    }
---
+++ src/org/tensorflow/demo/env/TextToSpeech.java
@@ -0,0 +1,17 @@
+package org.tensorflow.demo.env;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class TextToSpeech {
+
+android.speech.tts.TextToSpeech t1;
+
+
+
+}
+
